02a9ca754aecf1e5503df1f9eae12e097b180bcd
Victor Santelé <vic16@hotmail.be>
2021-03-12 12:02:23 +0100

    release 1.1.0

--- forge.config.js
+++ forge.config.js
@@ -38,7 +38,7 @@ module.exports = {
           "owner": "wolfvic",
           "name": "endurancepoint-electron"
         },
-        // "authToken": process.env.GITHUB_TOKEN
+        "authToken": process.env.GITHUB_TOKEN
       }
     }
   ],
